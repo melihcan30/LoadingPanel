96f3146bc265666373f1c5bb65b08811aaf8948d
acer <acer@DESKTOP-QDHOQ5E>
2021-06-27 22:27:08 +0300

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoadingPanel.MvcApplication" Language="C#" %>
---
+++ Scripts/APICall.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+    $("btnAPICall").click(function () {
+
+        $(".overlay,.popup").fadeIn();
+
+        CallAPI();
+
+    });
+
+
+    function LoadingBar() {
+        $("#Fade_area").removeAttr("style");
+        $("#myModal").removeAttr("style");
+    }
+
+
+    function CallAPI() {
+        $.ajax({
+            url: "",
+            type: 'post',
+            contentType: 'application/json',
+            success: function () {
+                setTimeout(function () { LoadingBar() }, 10000);
+            },
+            Error: function () {
+                setTimeout(function () { LoadingBar() }, 10000);
+            }
+        })
+    }
+
+
+});
